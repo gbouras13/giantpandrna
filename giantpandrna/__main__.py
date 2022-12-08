efd7ba3d854219e7d1d1fa388776694f4cbdec40
gbouras13 <george.bouras@adelaide.edu.au>
2022-12-08 12:09:31 +1030

    help

--- giantpandrna/__main__.py
+++ giantpandrna/__main__.py
@@ -132,7 +132,11 @@ def run(_input, output,species, log, **kwargs):
     )
 
 
-@click.command(epilog=help_msg_extra, help_option_names=["-h", "--help"], ignore_unknown_options=True)
+@click.command(
+    epilog=help_msg_extra,
+    context_settings=dict(
+        help_option_names=["-h", "--help"], ignore_unknown_options=True
+    ))
 @click.option('--species','species',  help='Species', show_default=True,  default='Rat',type=click.Choice(['Rat', 'Human']))
 def install(**kwargs):
     """Install databases"""
